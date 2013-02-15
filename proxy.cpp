616c7e2816d4ebde57becad0c6c0e752866174d3
Josh Gummersall <joshgummersall@gmail.com>
2013-02-14 23:46:29 -0800

    Quick and dirty cacheDump, needs work

--- proxy.cpp
+++ proxy.cpp
@@ -2,6 +2,9 @@
 #include "string.h"
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <istream>
 using namespace std;
 
 // Networking includes
@@ -18,6 +21,9 @@ const int HOST_INDEX = 1;
 const int PORT_INDEX = 2;
 const int BUF_SIZE = 80000;
 const bool PRODUCTION = false;
+const int CACHE_SIZE = 1000;
+const int CACHE_TTL = 5 * 60;
+const char * CACHE_FILE = "cacheDump.out";
 
 // Defaults
 char * DEFAULT_HOST = "www.google.com";
@@ -38,8 +44,18 @@ int acceptSocket(int sock);
 void hostnameToIp(char* host, char* ip);
 void sendSock(int sock, char * msg);
 char * recvSock(int sock);
+vector<CacheEntry> buildCache(); 
+void dumpCache(vector<CacheEntry> v);
 
 int main(int argc, char * argv[]) {
+
+  vector<CacheEntry> cache;
+  CacheEntry c1, c2, c3;
+  cache.push_back(c1);
+  cache.push_back(c2);
+  cache.push_back(c3);
+  dumpCache(cache);
+
   // Get user name and host name
   char * host, * port;
   bool args = validCmdArgs(argc, argv);
@@ -238,5 +254,33 @@ char * recvSock(int sock) {
     }
     pos += n;
   }
+}
+
+vector<CacheEntry> buildCache() {
+  ifstream inFile;
+  inFile.open(CACHE_FILE);
+  string request, headers, body, entry, accessed;
+  string line;
+
+  while (inFile.good()) {
+    getline(inFile, line);
+    cout << line << endl;
+  }
+
+  inFile.close();
+  inFile.clear();
+};
+
+void dumpCache(vector<CacheEntry> v) {
+  ofstream outFile;
+  outFile.open(CACHE_FILE);
+  if (!outFile.is_open()) {
+    cout << "Error opening file." << endl;
+  }
+  for (unsigned i = 0; i < v.size(); i++) {
+    outFile << v[i].toString();
+  }
+  outFile.close();
+  outFile.clear();
 }
 
